a350b44a91dffca9bb57dfede9388a3eac8e374d
charankamal20 <charanksingh20@gmail.com>
2024-01-26 01:09:41 +0530

    Add db.ts and user.ts files, update dependencies, and implement user registration

--- actions/register.ts
+++ actions/register.ts
@@ -2,14 +2,35 @@
 
 import { RegisterSchema } from "@/schemas";
 import * as z from "zod";
+import bcrypt from "bcrypt";
+import { db } from "@/lib/db";
+import { getUserByEmail } from "@/data/user";
 
 async function Register(values: z.infer<typeof RegisterSchema>) {
   const validatedFields = RegisterSchema.safeParse(values);
   if (!validatedFields.success) {
     return { error: "Invalid Fields!" };
   }
+  const {email, password, name} = validatedFields.data;
+  const hashedPass = await bcrypt.hash(password, 10);
 
-  return { success: "Email sent!" };
+  const existingUser = await getUserByEmail(email);
+
+  if(existingUser) {
+    return {error: "Email already in use!"};
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPass,
+    },
+  })
+
+  // TOdo Send verification token
+
+  return { success: "User Created!" };
 }
 
 export default Register;
